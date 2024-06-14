da3c05a51e177a2379467c7b032ef4d7fe9cddfb
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-06-14 12:29:55 -0700

    Set up express server, and added a routes folder for HTML and API routes

---
+++ server.js
@@ -0,0 +1,25 @@
+// set up of express server
+
+// import essential modules: 'express', 'path', 'fs', 'uuid', and route handlers for 'notes.js' and 'html.js'
+const express = require('express'); 
+const path = require('path'); 
+const fs = require('fs'); 
+const { v4: uuidv4 } = require('uuid'); 
+const apiRoutes = require('./routes/notes'); 
+const htmlRoutes = require('./routes/html'); 
+
+const app = express(); // create an Express application
+const PORT = 3001; // set the port to 3001
+
+// middleware
+app.use(express.json()); // parse incoming JSON requests
+app.use(express.urlencoded({ extended: true })); // parse URL-encoded requests
+app.use(express.static('public')); // serve static files from the 'public' directory
+
+// routes
+app.use('/api/notes', apiRoutes); // use API routes for '/api/notes'
+app.use('/', htmlRoutes); // use HTML routes for the root path
+
+app.listen(PORT, () => { // start the server
+  console.log(`API server now on port ${PORT}!`); // log the server start message
+});
